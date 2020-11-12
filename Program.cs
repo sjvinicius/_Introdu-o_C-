a78e3a5357d790ed03d35a8fa64739524af5bfbe
Vinicius <vinyloucoporfutebol@hotmail.com>
2020-11-12 02:14:57 -0300

    primeiro código C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _Introdução_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o 1o Número: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o 2o Número: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int resultado = n1 + n2;
+
+            Console.Write("O resultado de " + n1 + " + " + n2 + " é igual a " + resultado );
+        }
+    }
+}
