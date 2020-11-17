176ad2be17f67107c373c105ae32cdeb9cf06123
Vinicius <vinyloucoporfutebol@hotmail.com>
2020-11-17 11:04:03 -0300

    Exercicios de estruturas condicionais C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace _Introdução_C_
 {
@@ -6,15 +7,82 @@ namespace _Introdução_C_
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite o 1o Número: ");
-            int n1 = int.Parse(Console.ReadLine());
+                    // iNICIO EXERCICIO 1
+ 
+            // Console.WriteLine ("Qual é a sua idade ? (yy)");
+            // int idade = int.Parse(Console.ReadLine());
+ 
+            // int idadem      = idade * 12;
+            // int idaded      = idadem * 30;
+            // int idadeh      = idaded * 24;
+            // int idademin    = idadeh * 60;
+ 
+            // Console.WriteLine("Idade (Anos): " + idade);
+            // Console.WriteLine("Idade (Meses): " + idadem);
+            // Console.WriteLine("Idade (Dias): " + idaded);
+            // Console.WriteLine("Idade (Horas): " + idadeh);
+            // Console.WriteLine("Idade (Minutos): " + idademin);
 
-            Console.Write("Digite o 2o Número: ");
-            int n2 = int.Parse(Console.ReadLine());
+                    // FIM EXERCICIO 1
+            
+                // iNICIO EXERCICIO 2
+            
+            // Console.Write ("Ano atual (yyyy): ");
+            // int anoat = int.Parse(Console.ReadLine());
+            // Console.Write ("Ano de nascimento (yyyy): ");
+            // int anona = int.Parse(Console.ReadLine());
+    
+            // int idade = anoat - anona;
+            // int idadades = (idade * 12) * 4;
+    
+            // Console.WriteLine ("A sua idade é: " + idade);
+            // Console.WriteLine ("A sua idade em semanas é =~: " + idadades);
 
-            int resultado = n1 + n2;
+                // FIM EXERCICIO 2
 
-            Console.Write("O resultado de " + n1 + " + " + n2 + " é igual a " + resultado );
+            // iNICIO EXERCICIO 3
+            
+             
+            // Console.Write ("Por favor, insira o seu salário: R$");
+            // int salarioA = int.Parse(Console.ReadLine());
+            //     if (salarioA < 500) {
+ 
+            //         int juros = salarioA / 100 * 30;
+            //         int salarioP = salarioA + juros;
+ 
+            //         Console.WriteLine("Como o seu salário é: R$" + salarioA + " então você passará a receber R$" + salarioP + "." );
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Infelizmente o seu salário não é inferior à R$500, então voce continuará a receber: R$ " + salarioA);
+            //     }
+
+
+            // FIM EXERCICIO 3
+
+            // INICIO DESAFIO
+            // Console.WriteLine("-----------------------");
+            // Console.WriteLine("- Bem-Vindo - Natação -");
+            // Console.WriteLine("----------------------");
+            // Console.Write("Insira a sua idade: ");
+            // int idade = int.Parse(Console.ReadLine());
+
+            // if (idade >= 5 && idade <= 7 ) {
+            //     Console.WriteLine("De acordo com a sua idadede de " + idade + " anos. a sua categoria é Infantil A");
+            // }
+            // if (idade >= 8 && idade <= 10){
+            //     Console.WriteLine("De acordo com a sua idadede de " + idade + " anos. a sua categoria é Infantil B");
+            // }
+            // if (idade >= 11 && idade <= 13) {
+            //     Console.WriteLine("De acordo com a sua idadede de " + idade + " anos. a sua categoria é Juvenil A");
+            // }
+            // if (idade >= 14 && idade <= 17) {
+            //     Console.WriteLine("De acordo com a sua idadede de " + idade + " anos. a sua categoria é Juvenil B");
+            // }
+            // if (idade >= 18 && idade <= 99) {
+            //     Console.WriteLine("De acordo com a sua idadede de " + idade + " anos. a sua categoria é Sênior");
+            // }     FIM DESAFIO
+        
         }
     }
 }
